bbbb1428cb9ba5b2c2cc0193f6c3daeeaeffc137
Patrick Poopathi <patrickgp1992@hotmail.com>
2022-09-22 09:40:44 -0400

    completed user-routes will complete index.js for testing

---
+++ routes/api/user-routes.js
@@ -0,0 +1,26 @@
+const router = require("express").Router();
+
+const {
+  getAllUsers,
+  getUserById,
+  createUser,
+  updateUserById,
+  deleteUser,
+  addFriend,
+  removeFriend,
+} = require("../../controllers/user-controller");
+
+// GET and POST at /api/users
+router.route("/").get(getAllUsers).post(createUser);
+
+// GET, PUT, DELETE by ID at /api/users/:id
+router
+  .route("/:userId")
+  .get(getUserById)
+  .put(updateUserById)
+  .delete(deleteUser);
+
+// POST and DELETE at /api/users/:userId/friends/:friendId
+router.route("/:userId/friends/:friendId").post(addFriend).delete(removeFriend);
+
+module.exports = router;
