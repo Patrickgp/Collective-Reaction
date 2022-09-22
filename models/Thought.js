13f28baeb98aa7f34136410e67e2cb61dfe98a00
Patrick Poopathi <patrickgp1992@hotmail.com>
2022-09-22 08:44:44 -0400

    Completed Thought model. Will merge with develop branch and start working on routes

--- models/Thought.js
+++ models/Thought.js
@@ -1,17 +1,58 @@
 const { Schema, model, Types } = require("mongoose");
 
-const thoughtSchema = new Schema({
-  thoughtText: {
-    type: String,
-    required: true,
-    maxlength: 280,
-    // Must be between 1 and 280 characters
+const reactionSchema = new Schema(
+  {
+    reactionId: {
+      type: Schema.Types.ObjectId,
+      default: () => new Types.ObjectId(),
+    },
+    reactionBody: {
+      type: String,
+      required: true,
+      maxlength: 280,
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+    },
   },
-  username: {
-    type: String,
-    required: true,
+  {
+    toJSON: {
+      getters: true,
+    },
+  }
+);
+
+const thoughtSchema = new Schema(
+  {
+    thoughtText: {
+      type: String,
+      required: true,
+      maxlength: 280,
+      // Must be between 1 and 280 characters
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    reactions: [reactionSchema],
   },
-  reactions: {},
+  {
+    toJSON: {
+      virtuals: true,
+      getters: true,
+    },
+    id: false,
+  }
+);
+
+// Get the total count of reactions and replies
+thoughtSchema.virtual("reactionCount").get(function () {
+  return this.reactions.length;
 });
 
 const Thought = model("Thought", thoughtSchema);
